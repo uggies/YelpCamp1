e280aa78a9faff0e568459a32cae6a33467ebfad
Harry Noh <uggies@naver.com>
2021-04-25 06:31:45 +1000

    change 3000 to PORT in app.listen

--- app.js
+++ app.js
@@ -184,4 +184,4 @@ app.use((err, req, res, next)=> {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
